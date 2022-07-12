9ed420806f6629b0264567df426cf302261ab048
unknown <git.markan2nypascual@gmail.com>
2022-07-12 14:57:36 +0800

    Updated Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 
 export const Hero = () => {
   return (
     <div id="home" className="h-[100vh]">
-      <div className="container mx-auto">
-        <p className="text-slate-200">Hi, I'am</p>
-        <h1 className="text-3xl text-slate-700">Mark Anthony Pascual</h1>
+      <div className="md:container md:mx-auto h-full flex justify-center items-center flex-col text-center">
+        <p className="text-slate-400">Hi, I'am</p>
+        <h1 className="text-5xl text-slate-700 font-patua uppercase">
+          Mark Anthony Pascual
+        </h1>
+        <h1 className="text-3xl text-slate-600 font-[700]">
+          <Typewriter
+            options={{
+              autoStart: true,
+              loop: true,
+              delay: 40,
+              strings: ["Fullstack Web Developer", "Web Designer"],
+            }}
+          />
+        </h1>
       </div>
     </div>
   );
